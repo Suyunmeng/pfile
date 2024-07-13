fc57faafcd6bacc0f218951e81318d9f1ba4b45f
Suyunmeng <hua-honkai@qq.com>
2024-07-13 18:06:24 +0800

    Delete other directory

--- other/bingapi/utils.ts
+++
@@ -1,9 +0,0 @@
-export function parseQueryParams(searchParams: URLSearchParams): QueryParams {
-  return {
-    rand: searchParams.has("rand") ? searchParams.get("rand") === "true" : true,
-    day: searchParams.has("day") ? parseInt(searchParams.get("day") || "0", 10) : 0,
-    size: searchParams.get("size") || "1920x1080",
-    info: searchParams.has("info") ? searchParams.get("info") === "true" : false,
-    cn: searchParams.has("cn") ? searchParams.get("cn") === "true" : false,
-  };
-}
